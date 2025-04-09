02c137c30b26f664a1d4f74bf230e04ccb660973
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-04-09 16:28:33 -0300

    feat: add full task manager functions: create, read, complete, delete, clear

--- foundamental_2_task/task-manager/index.js
+++ foundamental_2_task/task-manager/index.js
@@ -1,41 +1,85 @@
-let task = "jnj"; // переменная с описанием задачи
-let completedTaskCount = 4; // переменная для счёта завершённых задач
+let tasks = [
+    {
+    title: "F2 practice",
+    description: "Complete final task",
+    isCompleted: false,
+    createdDate: new Date(2025, 4, 8),
+    completedDate: new Date()
+},
+    {
+        title: "Happy husband",
+        description: "Make a dinner for husband",
+        isCompleted: false,
+        createdDate: new Date(2025, 4, 9),
+        completedDate: new Date()
+    }
+];
+let completedTaskCount = 0;
+let completedTasks = [];
 
 function showTask() {
-    task === ""
-        ? console.log("Задача отсутствует")
-        : console.log(`Текущая задача: ${task}`);
+    if (tasks.length === 0) {
+        return "Задача отсутствует";
+    } else {
+        console.log("Текущие задачи:");
+
+        tasks.forEach((task, index) => {
+            console.log(`Задача № ${index + 1}:`);
+            for (let [key, value] of Object.entries(task)) {
+                console.log(`${key}: ${value}`);
+            }
+        });
+    }
 }
 
 function setTask() {
-    if (task !== "") {
-        console.log('Не могу добавить задачу, завершите или удалите предыдущую');
-    } else {
-        task = prompt("Введите новую задачу: ");
-        showTask();
+    const newTask = {
+        title: prompt("Заголовок задачи"),
+        description: prompt("Описание задачи"),
+        isCompleted: false,
+        createdDate: +prompt("Дата создания (timestamp)"),
+        completedDate: +prompt("Дата завершения (timestamp)")
+    };
+        tasks.push(newTask);
+        console.log(`Задача добавлена ${newTask}`);
+}
+
+function completeTask(index) {
+    const task = tasks[index];
+    if (!task) {
+        console.log("Такой задачи не существует.");
+        return;
+    }
+
+    task.isCompleted = true;
+    task.completedDate = new Date();
+
+    completedTasks.push(task);
+    tasks.splice(index, 1);
+    completedTaskCount++;
+    console.log(`Задача ${index + 1}: выполнена`);
+}
+
+function deleteTask(index) {
+    const task = tasks[index];
+    if (!task.isCompleted) {
+        const answer = prompt("Таска еще не выполнена, Удалить?")
+        if (answer === "Да") {
+            tasks.splice(index, 1);
+            console.log("Таска удалена")
+        } else {
+            console.log("Таска не удалена")
+        }
     }
 }
 
-function completeTask() {
-    if (task !== "") {
-        console.log(`Задача "${task}" завершена`);
-        task = "";
-        completedTaskCount++;
-    } else {
-        console.log("Нет  задач для завершения");
-    }
+function clearTasks() {
+    tasks.length = 0;
+    console.log("Все задачи удалены.");
 }
 
-function deleteTask() {
-    if (task !== "") {
-        console.log(`Задача "${task}" удалена`);
-        task = "";
-    } else {
-        console.log("Нет задач для удаления");
-    }
-}
-
-showTask();       // Вывод текущей задачи
-setTask();        // Устанавливаем новую задачу
-completeTask();   // Завершаем задачу
-deleteTask();     // Удаляем задачу
+showTask();
+setTask();
+completeTask();
+deleteTask();
+clearTasks();
