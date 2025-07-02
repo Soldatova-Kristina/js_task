c16ba153b68910b04b38b75201d7ffd0e44720b4
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-07-02 17:08:00 -0300

    refactor: uncomment code

--- array_methods/codewars_tasks/index.js
+++ array_methods/codewars_tasks/index.js
@@ -1,15 +1,17 @@
 // 1
 function filter_list(l) {
     return l
-        .filter(x => typeof x !== 'string' )
+        .filter(x => typeof x !== 'string')
 }
+
 // 2
-function findShort(s){
-        return s
-            .split(' ')
-            .reduce((longest, word) =>
+function findShort(s) {
+    return s
+        .split(' ')
+        .reduce((longest, word) =>
             word.length < longest.length ? word : longest).length
 }
+
 // 3
 function positiveSum(arr) {
     const getPositive = arr
@@ -17,64 +19,118 @@ function positiveSum(arr) {
     return getPositive
         .reduce((acc, num) => acc + num, 0)
 }
+
 // 4
 function getCount(str) {
-   return str
+    return str
         .split('')
-        .filter(i => i === 'a' || i === 'e' ||  i === 'i' || i === 'o' || i === 'u').length
+        .filter(i => i === 'a' || i === 'e' || i === 'i' || i === 'o' || i === 'u').length
 }
+
 // 5
-// function scrollingText(text) {
-//     return text
-//         .split('').reduce((acc, word, index) => {
-//             text.forEach(i => {
-//
-//             })
-//
-//         })
-//
-//     console.log(scrollingText('ABC'))
+function scrollingText(text) {
+    let result = [];
+    for (let i = 0; i < text.length; i++) {
+        const rotated = text.slice(i) + text.slice(0, i);
+        result.push(rotated);
+    }
+    return result;
+}
+
+console.log(scrollingText("codewars"))
 // 6
-    var capitals = function (word) {
-        return word
-            .split('')
-            .map((el, index) => el === el.toUpperCase() ? index : -1)
-            .filter(el => el !== -1)
-    }
+var capitals = function (word) {
+    return word
+        .split('')
+        .map((el, index) => el === el.toUpperCase() ? index : -1)
+        .filter(el => el !== -1)
+}
+
 // 7
-    function insertDash(num) {
-        const digits = num.toString().split('')
-        const result = digits.reduce((acc, el, index) => {
-            const current = Number(digits[index])
-            const prev = Number(digits[index - 1])
-            if (current % 2 !== 0 && prev % 2 !== 0) {
-                return acc + "-" + el
-            } else {
-                return acc + el
-            }
-        })
-        return result
-    }
+function insertDash(num) {
+    const digits = num.toString().split('')
+    const result = digits.reduce((acc, el, index) => {
+        const current = Number(digits[index])
+        const prev = Number(digits[index - 1])
+        if (current % 2 !== 0 && prev % 2 !== 0) {
+            return acc + "-" + el
+        } else {
+            return acc + el
+        }
+    })
+    return result
+}
+
 // 8
-    function filterHomogenous(arrays) {
-        return arrays.filter(arr => {
-            if (!Array.isArray(arr) || arr.length === 0)
-                return false
-            const identType = typeof arr[0];
-            return arr.every(el => typeof el === identType)
-        });
+function filterHomogenous(arrays) {
+    return arrays.filter(arr => {
+        if (!Array.isArray(arr) || arr.length === 0)
+            return false
+        const identType = typeof arr[0];
+        return arr.every(el => typeof el === identType)
+    });
+}
+
+//9
+
+function min(arr, toReturn) {
+    const minValue = Math.min(...arr)
+    if (toReturn === "index") {
+        return arr.indexOf(minValue)
+    } else {
+        return minValue;
     }
-// 9
+}
 
-// 10
+const array = [1, 2, 3, 4, 5]
+console.log(min(array, "value"))
 
-// 11
+//10
 
-// 12
+function generateShape(integer) {
+    const result = '+'.repeat(integer)
+    return Array(integer).fill(result).join('\n')
+}
+
+console.log(generateShape(8))
+
+//11
+
+function findMagic(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (i === arr[i]) return arr[i];
+    }
+    return -1;
+}
+
+const findMagic2 = arr => arr.findIndex(i => arr[i] === i);
+//12
+obfuscate = function (email) {
+    return email.split("")
+        .map((el) =>
+            el === "@" ? " [at] "
+                : el === "." ? " [dot] " : el)
+        .join("")
+}
 
 // 13
+function arrCheck(value) {
+    return Array.isArray(value) && value.every(Array.isArray);
+}
 
 // 14
+function smallEnough(a, limit) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > limit) {
+            return false
+        }
+    }
+    return true
+}
+
+function smallEnough2(a, limit) {
+    return Math.max(...a) <= limit
+}
 
 // 15
 
